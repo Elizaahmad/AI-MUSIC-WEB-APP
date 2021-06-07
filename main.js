978b530332790c91c4dca42bbc27263719698c31
Elizaahmad <74451187+Elizaahmad@users.noreply.github.com>
2021-06-07 15:04:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song="";
+
+function preload(){
+song=loadSound('Music.mp3');
+}
+function setup(){
+canvas=createCanvas(500,500);
+canvas.center();
+
+video=createCapture(VIDEO);
+video.hide();
+video.size(500,500);
+
+}
+function draw(){
+image(video,0,0,500,500);
+}
+function play(){
+song.play();
+}
